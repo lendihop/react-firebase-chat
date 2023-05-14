337ca3cee066bc3d34716e0daef15e5fb5c3d258
lendihop <mishin.danil2017@gmail.com>
2023-05-14 20:36:18 +0200

    chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,11 +5,41 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import {useCollectionData} from "react-firebase-hooks/firestore";
+import Loader from "./Loader";
+import {collection, addDoc, serverTimestamp} from "firebase/firestore";
+import Avatar from "@mui/material/Avatar";
 
 const Chat = () => {
-    const {auth, firestore} = useContext(Context);
+    const {auth, db} = useContext(Context);
     const [user] = useAuthState(auth);
     const [value, setValue] = useState('');
+
+    const [messages, loading] = useCollectionData(
+        collection(db, "messages")
+    );
+
+    const sendMessage = async () => {
+        try {
+            const docRef = await addDoc(
+                collection(db, "messages"), {
+                    uid: user.uid,
+                    displayName: user.displayName,
+                    photoURL: user.photoURL,
+                    text: value,
+                    createdAt: serverTimestamp()
+                });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+
+        setValue('');
+    }
+
+    if (loading) {
+        return <Loader/>
+    }
 
     return (
         <Container>
@@ -19,7 +49,21 @@ const Chat = () => {
                   style={{height: window.innerHeight - 50}}>
                 <div
                     style={{width: '80%', height: '70vh', border: '1px solid gray', overflowY: 'auto'}}>
-
+                    {messages.map(message =>
+                        <div key={message.createdAt} style={{
+                            margin: 10,
+                            border: user.uid === message.uid ? '2px solid green' : '2px dashed red',
+                            marginLeft: user.uid === message.uid ? 'auto' : 10,
+                            width: 'fit-content',
+                            padding: 5
+                        }}>
+                            <Grid container alignItems="center" gap={1}>
+                                <Avatar src={message.photoURL}/>
+                                <div>{message.displayName}</div>
+                            </Grid>
+                            <div>{message.text}</div>
+                        </div>
+                    )}
                 </div>
                 <Grid container
                       direction="column"
@@ -30,7 +74,7 @@ const Chat = () => {
                                variant="outlined"
                                value={value}
                                onChange={e => setValue(e.target.value)}/>
-                    <Button>Send</Button>
+                    <Button onClick={sendMessage}>Send</Button>
                 </Grid>
             </Grid>
         </Container>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 export const Context = createContext(null);
 
-const firebaseApp = initializeApp({
+const app = initializeApp({
     apiKey: "AIzaSyDkpyB6uGjKm0mMBo5euMIu1K2vrXIOc-M",
     authDomain: "react-firebase-chat-2ef23.firebaseapp.com",
     projectId: "react-firebase-chat-2ef23",
@@ -16,15 +16,15 @@ const firebaseApp = initializeApp({
     appId: "1:1091764846450:web:1ffdf3e36199bf52a727eb"
 });
 
-const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Context.Provider value={{
-      firebaseApp,
-      firestore,
-      auth
+      app,
+      auth,
+      db
   }}>
     <App />
   </Context.Provider>
